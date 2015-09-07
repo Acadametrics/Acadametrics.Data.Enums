ebe66dfa6596b14274e46100811f94b73f9a5cbe
Toby Worth <tobyworth@hotmail.com>
2015-09-07 20:06:36 +0100

    adding full CRUD

--- ClaimType.cs
+++ ClaimType.cs
@@ -7,17 +7,21 @@
 	{
 		Undefined = 0,
 		IsSystemAdmin = 1,
-		CanRead = 5,
-		CanWrite = 10, 
-		CanInsert = 11,
-		CanInsertUser = 12,
-		CanUpdate = 15,
-		CanUpdateUser = 16,
-		CanDelete = 20,
-		CanDeleteUser = 21,
-		CanEditTimeTable = 25,
-		CanEditHomework = 26,
+    // To simplify the authR structure, only use 'is a' claims.  What those claims mean is set at logic branch.
+		//CanRead = 5,
+		//CanWrite = 10, 
+		//CanInsert = 11,
+		//CanInsertUser = 12,
+		//CanUpdate = 15,
+		//CanUpdateUser = 16,
+		//CanDelete = 20,
+		//CanDeleteUser = 21,
+		//CanEditTimeTable = 25,
+		//CanEditHomework = 26,
 		IsPrincipal = 30,
-		IsInstitutionAdmin = 35
+		IsInstitutionAdmin = 35,
+    IsTeacher = 40,
+    IsParent = 45,
+    IsStudent = 50
 	}
 }
